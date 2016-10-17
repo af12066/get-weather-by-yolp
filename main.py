1a3667531d4f5a35ce858b2695f68632576b2004
af12066 <af12066@shibaura-it.ac.jp>
2016-10-17 15:00:27 +0900

    XML取得部分を追記

--- main.py
+++ main.py
@@ -2,12 +2,39 @@
 
 import requests
 
+
 class yolp():
+    '''YOLPに関するクラス'''
+
     def __init__(self, id):
+        '''初期化'''
         self.appid = id
-        print(self.appid)
+        self.main()
+
+    def setCoordinates(self):
+        '''緯度・経度の設定'''
+        self.__longitude = 139.900620
+        self.__latitude = 35.785166
+        return str(self.__longitude) + ',' + str(self.__latitude)
+
+    def generateURL(self):
+        '''リクエストURLの生成'''
+        uri = 'http://weather.olp.yahooapis.jp/v1/place?coordinates=' \
+                + self.setCoordinates() \
+                + '&appid=' \
+                + self.appid \
+                + '&output=xml'
+        return uri
+
+    def getXML(self):
+        '''URLをGETして天気に関するXMLを取得'''
+        res = requests.get(self.generateURL())
+        print(res.text)
+
+    def main(self):
+        '''ここに手順を書く'''
+        self.getXML()
 
 if __name__ == '__main__':
     application_id = 'アプリケーションID'
     yolp(application_id)
-
